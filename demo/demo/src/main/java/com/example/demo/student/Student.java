85c5b015c5edcdf146997e1bdbb1a8b962cf77ad
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-10-27 20:16:49 +0600

    Added transient and ability to calculate age by itself.

--- demo/demo/src/main/java/com/example/demo/student/Student.java
+++ demo/demo/src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity // For hibernate.
 @Table // For table in database.
@@ -23,7 +24,8 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
-    private Integer age;
+    @Transient
+    private Integer age; // No need to be a column in database
 
     public Student() {
 
@@ -32,23 +34,19 @@ public class Student {
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -84,7 +82,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
--- demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -18,15 +18,13 @@ public class StudentConfig {
             Student muntaha = new Student(
                     "Muntaha",
                     "muntaha.islam0019@gmail.com",
-                    LocalDate.of(1999, Month.JULY, 19),
-                    22
+                    LocalDate.of(1999, Month.JULY, 19)
             );
 
             Student sameha = new Student(
                     "Sameha",
                     "sameha.kamrul@gmail.com",
-                    LocalDate.of(2000, Month.DECEMBER, 2),
-                    22
+                    LocalDate.of(2000, Month.DECEMBER, 2)
             );
 
             repository.saveAll(
