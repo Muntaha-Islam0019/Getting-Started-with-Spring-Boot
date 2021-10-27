2f372755314d93c2358749f15d0e4616acbedd74
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-10-27 20:32:09 +0600

    Enhanced some comments and added a to-do.

--- demo/demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// This class is going to have all the resources for the API
+
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
     private final StudentService studentService;
 
+    // Using dependency injection to connect the StudentService to studentService automatically using spring.
+    // So the StudentService class has to be a spring bean (to be instantiated).
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
--- demo/demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -3,11 +3,14 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
-@Service // @Component works the same but Service is better because of semantics
+// Mainly responsible for the business logic.
+// In simple words; this will help to transit data between API layer to data access from database layer.
+// So basically, it's the service layer.
+
+// Telling that this class has to service as a spring bean.
+@Service // @Component works the same but Service is better because of semantics.
 public class StudentService {
 
     private final StudentRepository studentRepository;
